a65a7ff058e27cd463c46e71ac625d220a8eff6e
Kerkristof <kerkristof@gmail.Com>
2018-07-12 16:47:59 +0200

    ajout d'un projet nodejs app chat

--- revisionJuillet/socketIO/index.js
+++ revisionJuillet/socketIO/index.js
@@ -1,39 +1,62 @@
 // Quoi ça fait?
+
 // require indique le fichier à importer ds la var app ==>app contient un objet qui contient des params et des méthodes
 // Ici, on importe le fichier (objet) "express"
 // Why?  ==> parce qu'on en a besoin ;=)
-var app = require('express')();
-console.log(app);
+var appBehaviour = require('express')();
+// console.log(appBehaviour);
 
 // ******************************************************
 // Quoi ça fait??
 // Importe la méthode "Server" de l'objet "http" qui pernd en paramètre l'objet "app"
 // why?? ==> Prrrrrrrrrrrrrrtt???
-var http = require('http').Server(app);
+var httpServer = require('http').Server(appBehaviour);
 
 // ******************************************************
 // Quoi ça fait?
 // genere un objet io qui stocke socket.io et prend l'objet http en paramètre
-// Why?? ==> Bof
-var io = require('socket.io')(http);
+// Why?? ==> Pour gérer des connexions persistantes (qui viennent de httpServer)
+var ioManager = require('socket.io')(httpServer);
 
 // ******************************************************
 // variable qui stocke les messages historiques
-var history = "Message";
+var history = [];
+
+var getHeure = function (){
+    var d = new Date();
+    return d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+};
+
+var Message = function (message) {
+    this.auteur = "Alice";
+    this.message = message;
+    this.heure = getHeure();
+};
+
+var afficheMsg = function(indice) {
+    return history[indice].auteur +" dit: " + history[indice].message + " a " + history[indice].heure;
+};
+
 
 // ******************************************************
-// execute la méthode "get" de notre objet "app" qui prend comme paramètres,'/'
+// le jour ou la méthode GET est appelée de notre objet "app" qui prend comme paramètres,'/'
 //et execute la fct callback qui a comme paramètre req et res
-app.get('/', function (req, res) {
+appBehaviour.get('/', function (request, response) {
 
     // ******************************************************
-    // execute la méthode res.sendFile qui va charger le index.html dans le navigateur
-    res.sendFile(__dirname + '/index.html');
+    // execute la méthode res.sendFile qui va met le fichier dans la réponse au  navigateur
+    response.sendFile(__dirname + '/index.html');
 });
 
 // ******************************************************
 // si la connexion est active, alors la fct socket.on est executée
-io.on('connection', function (socket) {
+ioManager.on('connection', function (socket) {
+
+    // TODO: renvoyer l'historique à celui qui vient de se connecter
+    for (var i=0; i<history.length; i++) {
+        socket.emit('chat message',afficheMsg(i));
+    };
+
 
     // ******************************************************
     // declenche la fct io.emit avec le parametre msg
@@ -41,12 +64,14 @@ io.on('connection', function (socket) {
 
         // ******************************************************
         // envoie le msg ds la page index html
-        io.emit('chat message', msg);
+        var newMsg = new Message(msg);
+        history.push(newMsg);
+        ioManager.emit('chat message', afficheMsg(history.length-1));
     });
 });
 
 // Indique sur quel port on va travailler
-http.listen(3000, function () {
+httpServer.listen(3000, function () {
     // affiche le num du port dans la console
     console.log('listening on *:3000');
 });
